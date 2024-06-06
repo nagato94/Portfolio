78eb3de77e7803d12f0c3db634d073952024578b
Kaique Costa <kaiquenagato94@outlook.com>
2024-06-06 13:13:33 -0300

    portfolio kaique

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Project.create(title: "BOTJUR", description: "Um assistente jurídico virtual para o telegram feito em Python integrado com a API da OPENIA (chatgpt) que auxilia profissionais do direito no seu dia a dia", link: "https://botjur.com.br/", image: "botjurimage.jpeg")
+
+Project.create(title: "Rails Gameskaar", description: "Um marketplace de jogos onde você pode comprar e vender seus games preferidos", link: "https://rails-gameskaar-bddbe1d318a6.herokuapp.com/", image: "gameskaar.jpeg")
+
+Project.create(title: "Pet Dossier", description: "Um app em que você cadastra seu pet, faz o acompanhamento da saúde completa do seu bichinho e ainda pesquisa online os produtos preferidos dele para comprar e deixar na sua wishlist", link: "https://rails-pet-dossier-b82d4f7d70f5.herokuapp.com/", image: "petdossier.jpeg")
+
+Project.create(title: "Fly Blog", description: "Um blog feito em rails em que os usuários podem interagir de forma anônima ou logada com suporte para língua inglesa e portuguesa", link: "https://desafio-blog-maino-1c804f375126.herokuapp.com/", image: "flyblog.jpeg")
